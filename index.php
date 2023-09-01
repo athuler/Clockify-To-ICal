9f6c9c8553a2a29083259870fe04d74edccde7d1
athuler <athuler@uchicago.edu>
2023-09-01 23:02:46 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en" data-bs-theme="dark">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Clockify to iCal</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+		
+	</head>
+	<body class="container text-center">
+		<h1 class="mb-3 mt-3">Clockify to iCal</h1>
+		<h2 class="mb-3">See your tracked time in your calendar</h2>
+		<form id="input_form">
+			<div class="row mb-3 col-sm-6 offset-sm-3">
+				<label for="api_key" class="col-sm-auto col-form-label">Enter your Clockify API Key:</label>
+				<div class="col-sm-auto">
+					<input type="text" name="api_key" title="Clockify API Key" id="api_key" class="form-control" placeholder="API Key"/>
+				</div>
+			</div>
+			<div class="row mb-3 col-sm-6 offset-sm-3">
+				<label for="num_items" class="col-sm-auto col-form-label">How many items would you like displayed?</label>
+				<div class="col">
+					<input type="number" name="num_items" min="1" max="1000" value="500" id="num_items" class="form-control"/>
+				</div>
+			</div>
+		</form>
+		
+		
+		
+		<div id="reveal" style="display: none; transition: opacity 1s;">
+			<label for="ical_link">Add this link to your calendar application of choice</label><br/>
+			<div class="input-group mb-3">
+				<input name="ical_link" type="text" class="form-control" id="input_to_copy" readonly>
+				<button class="btn btn-outline-secondary" type="button" id="button-addon2" onclick="copyLink()">
+					<i class="bi bi-clipboard"></i>
+				</button>
+			</div>
+		</div>
+		
+		<br/><br/>
+		
+		<a href="https://donate.stripe.com/00g2969475Pd4iQ5kk" target="__blank"><button class="btn btn-primary mb-3">Support this project</button></a>
+		
+		<p>Bugs? Feature suggestions? <a href="mailto:hi@clockifytoical.com">Contact us</a>!</p>
+		
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+		
+		<script>
+			// Get the input elements
+			var apiKeyInput = document.getElementById("api_key");
+			var numItemsInput = document.getElementById("num_items");
+			var inputForm = document.getElementById("input_form");
+			var revealDiv = document.getElementById("reveal");
+			var inputToCopyInput = document.getElementById("input_to_copy");
+			
+			var newText = "";
+
+			// Event listener for the keyup event on the apiKeyInput element
+			inputForm.addEventListener("keyup", function() {
+				// Check if there is any text entered in the input element
+				if (apiKeyInput.value.length > 0) {
+					// Fade in the revealDiv element
+					revealDiv.style.display = "block";
+					revealDiv.classList.add("fade-in");
+
+					// Set the new text
+					newText = "https://clockifytoical.com/view?key=" + apiKeyInput.value + "&num_items=" + numItemsInput.value;
+					inputToCopyInput.value = newText;
+				} else {
+					// Fade out the revealDiv element
+					revealDiv.classList.remove("fade-in");
+				}
+			});
+			function copyLink() {
+				navigator.clipboard.writeText(newText);
+			}
+		</script>
+	</body>
+</html>
---
+++ view.php
@@ -0,0 +1,156 @@
+<?php
+
+// Initialization
+header("Content-Type: text/calendar;charset=utf-8");
+$curl = curl_init();
+
+if(!isset($_GET["key"])) {
+	exit("ERROR - Missing API Key");
+}
+$apiKey = $_GET["key"];
+
+$numItems = 50;
+if(isset($_GET["num_items"]) and is_int(intval($_GET["num_items"]))) {
+	$numItems = intval($_GET["num_items"]);
+}
+
+// Create calendar
+require_once("icalendar/zapcallib.php");
+$icalobj = new ZCiCal();
+
+
+// Get User's ID and Workspace ID
+
+$url = "https://api.clockify.me/api/v1/user";
+
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+curl_setopt_array($curl, [
+	CURLOPT_URL => $url,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => [
+			"X-Api-Key: $apiKey"
+	],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+
+
+
+if ($err) {
+	exit("ERROR (cURL) - " . $err);
+}
+
+if (curl_getinfo($curl)['http_code'] != 200) {
+	exit("ERROR - Incorrect API Key");
+}
+
+$res = json_decode($response, true);
+//echo var_dump($res);
+
+if (!isset($res["defaultWorkspace"]) or !isset($res["id"])) {
+	exit("ERROR - Wrong Clockify response! they lied to us:(");
+}
+
+$workspaceID = $res["defaultWorkspace"];
+$userID = $res["id"];
+
+// Get projects list
+
+$url = "https://api.clockify.me/api/v1/workspaces/$workspaceID/projects?page-size=1000";
+curl_setopt_array($curl, [
+	CURLOPT_URL => $url,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => [
+			"X-Api-Key: $apiKey"
+	],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+if ($err) {
+	exit("ERROR (cURL) - " . $err);
+}
+
+if (curl_getinfo($curl)['http_code'] != 200) {
+	exit("ERROR - No Projects!");
+}
+
+$res = json_decode($response, true);
+$projects = array();
+foreach ($res as $project) {
+	$projects[$project["id"]] = $project["name"];
+}
+//var_dump($projects);
+
+
+
+
+// Get time entries
+
+
+$url = "https://api.clockify.me/api/v1/workspaces/$workspaceID/user/$userID/time-entries?page-size=$numItems&in-progress=False";
+
+curl_setopt_array($curl, [
+	CURLOPT_URL => $url,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => [
+			"X-Api-Key: $apiKey"
+	],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+
+
+
+if ($err) {
+	exit("ERROR (cURL) - " . $err);
+}
+
+if (curl_getinfo($curl)['http_code'] != 200) {
+	exit("ERROR - Something weird went wrong");
+}
+
+$res = json_decode($response, true);
+//echo var_dump($res);
+foreach ($res as $time_entry) {
+	
+	/*echo "Title: " . $projects[$time_entry["projectId"]] . "<br/>";
+	echo "Description: " . $time_entry["description"] . "<br/>";
+	echo "Start: " . $time_entry["timeInterval"]["start"] . "<br/>";
+	echo "End: " . $time_entry["timeInterval"]["end"] . "<br/><br/>";*/
+	
+	$eventobj = new ZCiCalNode("VEVENT", $icalobj->curnode);
+	$eventobj->addNode(new ZCiCalDataNode("SUMMARY:" . $projects[$time_entry["projectId"]]));
+	$eventobj->addNode(new ZCiCalDataNode("DESCRIPTION:" . $time_entry["description"]));
+	$eventobj->addNode(new ZCiCalDataNode("DTSTART:" . ZCiCal::fromSqlDateTime($time_entry["timeInterval"]["start"])));
+	$eventobj->addNode(new ZCiCalDataNode("DTEND:" . ZCiCal::fromSqlDateTime($time_entry["timeInterval"]["end"])));
+	$eventobj->addNode(new ZCiCalDataNode("DTSTAMP:" . ZCiCal::fromSqlDateTime()));
+	$uid = date('Y-m-d-H-i-s') . random_int(111111,1000000000000) . "@clockifytoical.com";
+	$eventobj->addNode(new ZCiCalDataNode("UID:" . $uid));
+}
+
+
+// Close
+curl_close($curl);
+echo $icalobj->export();
+?>
